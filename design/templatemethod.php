c75f4c96a3031054eca62693c5fea80d83542a96
shupan <shupan@didichuxing.com>
2021-03-27 18:01:48 +0800

    add template method

---
+++ design/templatemethod.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * @Date  : 3/27/21 5:47 PM
+ * @Author: shupan
+ * @File  : templatemethod.php
+ * @Desc  : 模板方法
+ */
+
+/**
+ * 模板抽象类
+ * Class AbstractClass
+ */
+abstract class AbstractClass {
+
+    //做菜
+    public function TemplateMethod() {
+
+        $this->method1(); //准备食材
+        $this->method2(); //炒菜
+    }
+
+    abstract protected function method1();
+
+    abstract protected function method2();
+
+}
+
+/**
+ * 具体实现类，西红柿鸡蛋
+ * Class ConcreteClass1
+ */
+class ConcreteClass1 extends AbstractClass {
+    protected function method1() {
+        echo "prepare tomato and egg\n";
+    }
+
+    protected function method2() {
+        echo "cook with small fire\n";
+    }
+}
+
+/**
+ * 具体实现类，做鱼
+ * Class ConcreteClass2
+ */
+class ConcreteClass2 extends AbstractClass {
+    protected function method1() {
+        echo "prepare fish\n";
+    }
+
+    protected function method2() {
+        echo "cook with big fire and took a long time";
+    }
+}
+
+
+$obj1 = new ConcreteClass1();
+$obj1->TemplateMethod();
+
+$obj2 = new ConcreteClass2();
+$obj2->TemplateMethod();
